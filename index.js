3c4b418c03b0986985576396c66b91e100107542
SUNDARAVEL7203 <sundarvel.kasi@gmail.com>
2025-04-03 12:03:16 +0530

    index.js modifications done

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
-import express from 'express'
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
-import userRoute from './routes/user.routes.js'
-import companyRoute from "./routes/company.routes.js";
-import jobRoute from "./routes/job.routes.js";
-import applicationRoute from "./routes/application.routes.js"
+import userRoute from './routes/user.routes.js';
+import companyRoute from './routes/company.routes.js';
+import jobRoute from './routes/job.routes.js';
+import applicationRoute from './routes/application.routes.js';
+
 const app = express();
+dotenv.config();
 
-dotenv.config()
-
+// Root route
 app.get("/", (req, res) => {
     return res.status(200).json({
-        message:"I am from backend server",
-        success : true
-    })
-})
+        message: "I am from backend server",
+        success: true
+    });
+});
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
-
+// CORS setup
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://jobportal-frontend-6x9ucndor-sundaravels-projects-633e20b1.vercel.app/'
-  ];
-  
-  app.use(cors({
+    'https://jobportal-frontend-6x9ucndor-sundaravels-projects-633e20b1.vercel.app'
+];
+
+app.use(cors({
     origin: allowedOrigins,
-    credentials: true,  // Enable cookies and other credentials across origins
-  }));
+    credentials: true
+}));
 
-
-const PORT = process.env.PORT || 5000;
-
-app.use("/api/v1/user", userRoute)
+// Routes
+app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute)
-app.use("/api/v1/application", applicationRoute)
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-app.listen(PORT, () =>{
-    connectDB()
+// Server listen
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
-})
+});
 
 //09asYq2yYaXsNOKa
